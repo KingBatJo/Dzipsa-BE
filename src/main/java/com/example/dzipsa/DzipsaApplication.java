f2aff0df9322598868dcd885ce3f9d2d80d88089
chaeun99 <hyc9328@naver.com>
2026-02-25 22:28:10 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/dzipsa/DzipsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dzipsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzipsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzipsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dzipsa/DzipsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dzipsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzipsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
