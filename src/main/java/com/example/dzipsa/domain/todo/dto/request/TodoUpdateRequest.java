940d20b5558ed083ed38d437abbde01808ae30ae
chaeun99 <hyc9328@naver.com>
2026-03-18 09:35:45 +0900

    feat: TodoUpdateRequest 구현 (#28)

---
+++ src/main/java/com/example/dzipsa/domain/todo/dto/request/TodoUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.dzipsa.domain.todo.dto.request;
+
+import com.example.dzipsa.domain.todo.entity.enums.RecurringType;
+import lombok.Getter;
+import java.time.LocalDate;
+
+@Getter
+public class TodoUpdateRequest {
+  private String title;
+  private String memo;
+  private Long assigneeId;
+  private RecurringType recurringType;
+  private String repeatDays;
+  private LocalDate endDate;
+}
