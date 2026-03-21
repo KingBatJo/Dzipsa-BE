d3086682beedd9cd5e599f293ad1b000e0358896
chaeun99 <hyc9328@naver.com>
2026-03-22 00:46:13 +0900

    feat: TodoCreateResponse 구현 (#26)

---
+++ src/main/java/com/example/dzipsa/domain/todo/dto/response/TodoCreateResponse.java
@@ -0,0 +1,21 @@
+package com.example.dzipsa.domain.todo.dto.response;
+
+import com.example.dzipsa.domain.todo.entity.enums.RecurringType;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class TodoCreateResponse {
+  private Long todoId;
+  private Long instanceId;
+  private String title;
+  private String memo;
+  private Long defaultAssigneeId;
+  private Boolean isRandom;
+  private RecurringType recurringType;
+  private String repeatDays;
+  private LocalDate startDate;
+  private LocalDate endDate;
+}
